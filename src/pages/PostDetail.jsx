8cb8123b5f624bfb2957048ea9f45220ecef2b1a
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-04-02 21:58:51 +0200

    Add PostDetail component to display individual post information

---
+++ src/pages/PostDetail.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const PostDetail = () => {
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data);
+      })
+      .catch((err) => {
+        setError("Error", err.message);
+      });
+  }, [postId]);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={() => navigate(-1)}>Torna indietro</button>
+    </div>
+  );
+};
+
+export default PostDetail;
